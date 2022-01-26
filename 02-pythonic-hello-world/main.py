b8dafb9d46dcd8a497611e7e1ba7777cb92590ba
unknown <andreea.abudaher@gmail.com>
2022-01-26 17:30:57 +0200

    Curs02

---
+++ 02-pythonic-hello-world/main.py
@@ -0,0 +1,58 @@
+# print("Numele meu este Andreea")
+
+# print("Tipuri de date Numere")
+# print (1,type(1)) # tipul int (intreg)
+# print (1.1,type(1.1)) # type float (rational/zecimal) . sistem american este cel pe care il foloseste python
+# print (1j,type(1j)) # type complex
+
+# print("Conversii Numere")
+# print(int(5.6))
+# print(float(1))
+# print(complex(1))
+
+# print("Operatori aritmetici")
+# print (1+1) # adunare
+# print (1-1) # scadere
+# print (1*3) # inmultire
+# print (5/2) # impartire exacta
+# print (5//2) # catul
+# print (5 % 2) # restul => se numeste modulo in programare
+# print (5**2) # ridicare la putere
+
+# print("operatori de comparare")
+# print (5 == 2) # operator de egalitate => returneaza un boolean ( True 1, False 0 )
+# print (5 != 2) # operator de diferentiere => returneaza boolean ( True sau False)
+# print (5 > 2)
+# print (5 < 2)
+# print (5 >= 2)
+# print (5 <= 2)
+# print(5 > 2 and 3 >1 )
+# print (5 > 2 or 3 > 1)
+# print ( not(5 > 2 and 3 >1)) # operator de negare
+# print ( 7 is 7)
+# print (7 is not 7)
+
+# print("Variabile")
+# my_var = 0
+# my_var, my_var_1= 0, 1
+#my_var = 2
+# my_var +=1 # asignare prin adunare / incrementare
+#my_var *=2
+#print(my_var)
+
+# print("String")
+# print ("Ana are mere")
+# print ('Ana are mere')
+
+# variabila = "Ana 'are' mere"
+# variabila = "Ana \'are\' mere"
+# print (variabila)
+# nume = "Abu"
+# prenume = "Andreea"
+# prezentare = "Numele meu este {1} si prenumele este {0} ".format(nume, prenume) # 0 si 1 se numesc indexi
+#prezentare = f"Numele meu este {nume} si prenumele meu este {prenume}. "
+# print (prezentare)
+# adunam doua stringuri se numeste concatenare
+# adunam 2 variabile de tip int este un simplu calcul matematic
+# daca adunam int + float va rezulta un float
+
