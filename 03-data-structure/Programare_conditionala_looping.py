d36e96e69babad5711b9f0c171d849f368cea4eb
unknown <andreea.abudaher@gmail.com>
2022-01-27 16:43:57 +0200

    Curs03

---
+++ 03-data-structure/Programare_conditionala_looping.py
@@ -0,0 +1,42 @@
+# daca conditie :#
+# print ("adevarat") # indentare hit Tab
+
+# my_var = 11
+# if my_var < 6:
+#         print("Set instructiuni #1")
+# elif my_var < 10:
+#         print("Set instructiuni #2")
+# else:
+#         print("Set intrustiuni #3")
+# print("a iesit")
+
+
+# my_var = 4
+# mesaj = "Set instructiuni #3"
+# if my_var < 6:
+#         mesaj = "Set instructiuni #1"
+# elif my_var < 10:
+#         mesaj = "Set onstructiuni #2"
+# print (mesaj)
+# print("a iesit")
+
+# Strucuturi repetitive
+
+# while  True:
+#         print("bloc de instructiuni")
+#         break
+# var = 1
+# while var < 10:
+#
+#         print("bloc de instructiuni", var)
+#         var += 1
+#         break
+
+# string = "Ana are mere"
+# lista = [1, 2, 3, "a"]
+# for i, v in enumerate(lista):
+#         print(i,'=>',v)
+# for variabila_temporara in range(0,len(lista)):
+#          print(lista[variabila_temporara])
+# print(variabila_temporara,">>")
+
