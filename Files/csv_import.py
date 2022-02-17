bcab5c0f620c78b7cdf38685703822b4e482f020
AbuDaher <97911697+AbuDaher@users.noreply.github.com>
2022-02-17 11:18:49 +0200

    Delete Files directory

--- Files/csv_import.py
+++
@@ -1,9 +0,0 @@
-import csv
-masini = [
-    ["Dacia", "Logan", 2005, 75],
-    ["renault", "cLIO", 2005, 75]
-]
-with open("data.csv", "a") as csv_file:
-    csv_writer = csv.writer(csv_file, delimiter=",")
-    for masina in masini:
-        csv_writer.writerow(masina)
