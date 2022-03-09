b2e97a763f5c61c9a926e9356e4018dd5888b674
AbuDaher <97911697+AbuDaher@users.noreply.github.com>
2022-03-09 21:36:06 +0200

    Delete 08_oop directory

--- 08_oop/mosteniri.py
+++
@@ -1,7 +0,0 @@
-# 1. Vehicul --> super clasa penru 1.1->1.4.
-# 1.1 Vehicul de apa --> sunt subclasa pentru 1
-# 1.2 Vehicul de aer
-# 1.3 Vehicul de spatiu
-# 1.4 Vehicul terestru
-# 1.4.1. Vehicul de teren
-# 1.4.1 Vehicul de cursa
