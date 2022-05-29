52fb01c481d7467a1cbf21b5e90a7dbffa01fc01
unknown <andreea.abudaher@gmail.com>
2022-05-29 18:14:22 +0300

    proiectfinal

--- proiectfinal/aplicatie1/urls.py
+++ proiectfinal/aplicatie1/urls.py
@@ -3,9 +3,14 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
     path('sign-up', views.sign_up, name='sign_up'),
-    path('create_post', views.create_post, name='create_post'),
+    path('create_post_actuals', views.create_post_actuals, name='create_post_actuals'),
+    path('create_post_planning', views.create_post_planning, name='create_post_planning'),
+    path('actuals',views.actuals, name='actuals'),
+    path('planning',views.planning, name='planning'),
+
 ]
--- proiectfinal/aplicatie1/views.py
+++ proiectfinal/aplicatie1/views.py
@@ -35,20 +35,92 @@ def home(request):
     return render(request, 'aplicatie1/home.html', {"posts" : posts})
 
 
+@login_required(login_url='/login')
+def actuals(request):
+    posts1 = Post.objects.all()
+
+    if request.method == "POST":
+        post_id = request.POST.get("post-id")
+        user_id = request.POST.get("user-id")
+
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            if post and (post.author == request.user or request.user.has_perm("aplicatie1.delete_post")):
+                post.delete()
+        elif user_id:
+            user= User.objects.filter(id=user_id).first()
+            if user and request.user.is_staff:
+                try:
+                    group = Group.objects.filter(name="default")
+                    group.user_set.remove(user)
+                except:
+                    pass
+                try:
+                    group = Group.objects.filter(name="mod")
+                    group.user_set.remove(user)
+                except:
+                    pass
+
+    return render(request, 'aplicatie1/actuals.html', {"posts1" : posts1})
+
+
+def planning(request):
+    posts2 = Post.objects.all()
+
+    if request.method == "POST":
+        post_id = request.POST.get("post-id")
+        user_id = request.POST.get("user-id")
+
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            if post and (post.author == request.user or request.user.has_perm("aplicatie1.delete_post")):
+                post.delete()
+        elif user_id:
+            user= User.objects.filter(id=user_id).first()
+            if user and request.user.is_staff:
+                try:
+                    group = Group.objects.filter(name="default")
+                    group.user_set.remove(user)
+                except:
+                    pass
+                try:
+                    group = Group.objects.filter(name="mod")
+                    group.user_set.remove(user)
+                except:
+                    pass
+
+    return render(request, 'aplicatie1/planning.html', {"posts2" : posts2})
+
+
 @login_required(login_url="/login")
-@permission_required("main.add_post", login_url="/login", raise_exception=True)
-def create_post(request):
+@permission_required("aplicatie1.add_post", login_url="/login", raise_exception=True)
+
+
+def create_post_actuals(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect("/home")
+            return redirect("/actuals")
     else:
         form = PostForm()
 
-    return render(request, 'aplicatie1/create_post.html', {"form": form})
+    return render(request, 'aplicatie1/create_post_actuals.html', {"form": form})
+
+def create_post_planning(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("/planning")
+    else:
+        form = PostForm()
+
+    return render(request, 'aplicatie1/create_post_planning.html', {"form": form})
 
 
 
